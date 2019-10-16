5903b03cd19d038cdde050d64eaf4f25ac39ca26
Kishor Naik <kishor.naik011.net@outlook.com>
2019-10-16 13:35:45 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Customer_Module/Areas/CustomerModule/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sol_Customer_Module.Areas.CustomerModule.Controllers
+{
+    [Area("CustomerModule")]
+    public class CustomerController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Data = TempData["CustomerData"] as string;
+
+            ViewBag.Data1 = TempData["CustomerData1"] as string;
+
+            return View();
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Web.Models;
+
+namespace Sol_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CallCustomerModule()
+        {
+            TempData["CustomerData"] = "Hello From Home Controller";
+
+            return RedirectToAction("Index", "Customer", new { Area = "CustomerModule" });
+        }
+
+        public IActionResult CallAdminModule()
+        {
+            TempData["AdminData"] = "Hello From Home Controller";
+
+            return RedirectToAction("Index", "Admin", new { Area = "AdminModule" });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
